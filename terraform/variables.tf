ff4732a60cd52661123331f09adf1c1a330bbf91
HashemAlH15 <hashemalh15@gmail.com>
2026-06-11 22:28:57 +0300

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+# AWS credentials
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+# Azure credentials
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+variable "azure_subscription_id" {}
+variable "azure_tenant_id" {}
+
+# ACM certificate for CloudFront (us-east-1)
+variable "acm_certificate_arn" {}
